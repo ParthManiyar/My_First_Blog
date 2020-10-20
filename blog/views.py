b9df088137b7c2911be28fee2840ca08509a0a15
unknown <parth.m@ahduni.edu.in>
2020-10-20 13:37:58 +0530

    Second

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,37 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.models import User
 from django.utils import timezone
 from django.shortcuts import get_object_or_404
 from .form import PostForm
 from django.shortcuts import redirect
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+
+class post_listAPI(APIView):
+    def get(self,request,*args,**kwargs):
+        authors = User.objects.all()
+        if(request.GET.__contains__('author')):
+            name = request.GET['author']
+        else:
+            name = 'All'
+
+        if(name=='All'):
+            posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+        else:
+            author_id = get_object_or_404(User,username=name)
+            posts = Post.objects.filter(author = author_id).order_by('published_date')
+
+        return render(request, 'blog/post_list.html', {'posts': posts,'authors':authors})
+
+post_list = post_listAPI.as_view()
+    
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
